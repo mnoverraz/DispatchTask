aac641ab394d5f49c0be29f5fd4dabaed45e3f75
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2015-04-06 20:28:09 +0200

    inital commit

---
+++ js/tables.js
@@ -0,0 +1,13 @@
+
+tableName = "table";
+table = document.getElementById(tableName);
+
+
+function createTable(){
+	console.log(table);
+    /*var header = table.createTHead();
+    var row = header.insertRow(0);
+    var cell = row.insertCell(0);*/
+}
+
+createTable();
