00537cda0e90b2ede8c7f4a4f4254ed6df6f3e31
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2015-04-07 11:05:01 +0200

    Ajout de la fonction sauvegarde

--- js/tables.js
+++ js/tables.js
@@ -33,4 +33,39 @@ function createContent(){
 	
 }
 
+function getJsonOutput(){
+	return JSON.stringify(new Array(dates,postes,eleves));
+}
+
+function save(){
+	(function () {
+		var textFile = null;
+		var makeTextFile = function (text) {
+		var data = new Blob([text], {type: 'application/json'});
+
+		if (textFile !== null) {
+		  window.URL.revokeObjectURL(textFile);
+		}
+
+		textFile = window.URL.createObjectURL(data);
+
+		return textFile;
+		};
+
+
+		var create = document.getElementById('saveLink');
+		create.addEventListener('click', function () {
+			var link = document.getElementById('saveLink');
+			link.href = makeTextFile(getJsonOutput());
+		}, false);
+	})();
+}
+
+
+
+
+
+
+
+
 createTable();
