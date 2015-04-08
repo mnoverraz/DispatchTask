1a16fdc385ad202593523723ec74334b82f797ca
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2015-04-08 17:53:36 +0200

    Ajout du drop. L'affichage ne fonctionne pas. Le contenu du fichier peut être lu avec reader.result

--- js/tables.js
+++ js/tables.js
@@ -62,9 +62,75 @@ function save(){
 	})();
 }
 
+/*function handleFileSelect(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+
+	var files = evt.dataTransfer.files; // FileList object.
+
+	// files is a FileList of File objects. List some properties.
+	var output = [];
+	for (var i = 0, f; f = files[i]; i++) {
+		output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+			f.size, ' bytes, last modified: ',
+	    	f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+	    '</li>');
+	}
+	document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+}*/
+
+
+function handleFileSelect(evt) {
+	console.log('handleFileSelect');
+	evt.stopPropagation();
+	evt.preventDefault();
+
+	files = evt.dataTransfer.files; // FileList object.
+	theFile = files[0];
+
+	reader = new FileReader();
+	reader.onload = function(e) {
+		alert(e.target.result);
+	}
+	reader.readAsText(theFile);
+
+      // Closure to capture the file information.
+      /*reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+          document.getElementById('drop_zone').insertBefore(span, null);
+        };
+      })(f);*/
+
+      // Read in the image file as a data URL.
+      //reader.readAsDataURL(f);
+
+}
 
 
 
+
+
+
+
+
+
+
+
+function handleDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+
+// Setup the dnd listeners.
+var dropZone = document.getElementById('drop_zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
 
 
 
